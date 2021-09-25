d30c7038c7effc875588265ab004b843349d44e7
Jacob Oxley <jdoxley6@gmail.com>
2021-09-25 15:53:17 -0700

    Spelling fix

--- addons/vehicles/UH60AceActions.hpp
+++ addons/vehicles/UH60AceActions.hpp
@@ -15,10 +15,10 @@ class ACE_SelfActions: ACE_SelfActions {
 			statement = "_target setObjectTextureGlobal [0, 'rhsusf\addons\rhsusf_a2port_air\uh60m\data\uh60m_fuselage_co.paa']; _target setObjectTextureGlobal [1, 'rhsusf\addons\rhsusf_a2port_air\uh60m\data\uh60m_engine_co.paa']";
 		};
 		// noseArt(uhFG1.paa, uhEG1.paa, test, Test)
-		noseArt(P6F.paa, E.paa, p6, Pheonix 6)
-		noseArt(P11F.paa, P11E.paa, p11, Pheonix 1-1)
-		noseArt(P12F.paa, E.paa, p12, Pheonix 1-2)
-		noseArt(P13F.paa, E.paa, p13, Pheonix 1-3)
-		noseArt(P14F.paa, E.paa, p14, Pheonix 1-4)
+		noseArt(P6F.paa, E.paa, p6, Phoenix 6)
+		noseArt(P11F.paa, P11E.paa, p11, Phoenix 1-1)
+		noseArt(P12F.paa, E.paa, p12, Phoenix 1-2)
+		noseArt(P13F.paa, E.paa, p13, Phoenix 1-3)
+		noseArt(P14F.paa, E.paa, p14, Phoenix 1-4)
 		};
 };
