5fc03f169c3ed8165489d0f8a26fd0ea40b8a3ac
Jacob Oxley <jdoxley6@gmail.com>
2021-10-25 13:14:34 -0700

    add uniforms

---
+++ addons/uniforms/config.cpp
@@ -0,0 +1,36 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = QUOTE(COMPONENT);
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {};
+        author = "AUTHOR";
+        VERSION_CONFIG;
+    };
+};
+
+class CfgWeapons {
+    class rhs_uniform_acu_ucp;
+    class rhs_uniform_acu_oefcp : rhs_uniform_acu_ucp {
+        class ItemInfo;
+    };
+    class NEW_UNIFORM_WEP(inf,base) : rhs_uniform_acu_oefcp {
+        displayName = "[61st] Infantry Uniform";
+        class ItemInfo : ItemInfo {
+            uniformClass = QUOTE(NEW_UNIFORM(inf,base));
+        };
+    };
+    NEW_INF_UNIFORM_WEP(myers)
+};
+
+
+class CfgVehicles {
+    class rhsusf_army_acu_oefcp_uniform;
+    class NEW_UNIFORM(inf,base) : rhsusf_army_acu_oefcp_uniform {
+        hiddenSelectionsTextures[] = {QPATHTOF(_textures\inf\base.paa),"rhsusf\addons\rhsusf_infantry2\acu\data\rhsusf_uniform_acu_02_oefcp_co.paa","rhsusf\addons\rhsusf_infantry2\acu\data\rhsusf_uniform_acu_03_ucp_co.paa","#(argb,8,8,3)color(0,0,0,0)","rhsusf\addons\rhsusf_infantry2\acu\data\rhsusf_uniform_acu_acc2_co.paa"};
+    };
+    NEW_INF_UNIFORM(myers)
+};
---
+++ addons/uniforms/script_component.ht.hpp
@@ -0,0 +1,34 @@
+#define COMPONENT uniforms
+#include "\{{mainprefix}}\{{prefix}}\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+
+#ifdef DEBUG_ENABLED_UNIFORMS
+    #define DEBUG_MODE_FULL
+#endif
+    #ifdef DEBUG_SETTINGS_OTHER
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_UNIFORMS
+#endif
+
+#include "\{{mainprefix}}\{{prefix}}\addons\main\script_macros.hpp"
+
+#define NEW_UNIFORM(grp,name) GVAR(TRIPLES(grp,name,uniform))
+#define NEW_UNIFORM_WEP(grp,name) GVAR(DOUBLES(grp,name))
+
+#define NEW_INF_UNIFORM(name) \
+    class NEW_UNIFORM(inf,name) : NEW_UNIFORM(inf,base) { \
+        scope = 2; \
+        scopeCurator = 2; \
+        hiddenSelectionsTextures[] = {QPATHTOF(_textures\inf\##name##.paa),"rhsusf\addons\rhsusf_infantry2\acu\data\rhsusf_uniform_acu_02_oefcp_co.paa","rhsusf\addons\rhsusf_infantry2\acu\data\rhsusf_uniform_acu_03_ucp_co.paa","#(argb,8,8,3)color(0,0,0,0)","rhsusf\addons\rhsusf_infantry2\acu\data\rhsusf_uniform_acu_acc2_co.paa"}; \
+    };
+
+#define NEW_INF_UNIFORM_WEP(name) \
+    class NEW_UNIFORM_WEP(inf,name) : NEW_UNIFORM_WEP(inf,base) { \
+        scope = 2; \
+        scopeCurator = 2; \
+        displayName = QUOTE([61st] Infantry Uniform ('##name##')); \
+        class ItemInfo : ItemInfo { \
+            uniformClass = QUOTE(NEW_UNIFORM(inf,name)); \
+        }; \
+    };
