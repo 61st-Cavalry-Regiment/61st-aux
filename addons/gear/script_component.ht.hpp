01f34ff7d5992d7c8c0b38d3def43078a6e3c925
Jacob Oxley <jdoxley6@gmail.com>
2021-11-10 11:29:59 -0800

    disable a debug mode

--- addons/gear/script_component.ht.hpp
+++ addons/gear/script_component.ht.hpp
@@ -1,13 +1,13 @@
 #define COMPONENT gear
 #include "\{{mainprefix}}\{{prefix}}\addons\main\script_mod.hpp"
 
-#define DEBUG_MODE_FULL
+// #define DEBUG_MODE_FULL
 // #define DISABLE_COMPILE_CACHE
 
 #ifdef DEBUG_ENABLED_GEAR
     #define DEBUG_MODE_FULL
 #endif
-    #ifdef DEBUG_SETTINGS_OTHER
+#ifdef DEBUG_SETTINGS_OTHER
     #define DEBUG_SETTINGS DEBUG_SETTINGS_GEAR
 #endif
 
