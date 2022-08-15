97b687d54696a7e9b0e65d38078708da571821d0
Drivyn <94076829+Drivyn@users.noreply.github.com>
2022-08-15 14:59:04 -0400

    Remove RHS as required Addon (server crash)

--- addons/vehicles/config.cpp
+++ addons/vehicles/config.cpp
@@ -3,10 +3,10 @@
 class CfgPatches {
     class ADDON {
         name = COMPONENT_NAME;
-        units[] = {"RHS_UH60M", QGVAR(UH60m_DAP), QGVAR(3R), QGVAR(MELB_MH6M), QGVAR(MELB_AH6M), QGVAR(AH64D_wd), QGVAR(CH_47F), QGVAR(UH60M), GVAR(pinkhawk), "RHS_M2A3_BUSKIII"};
+        units[] = {"61ST_UH60M_DAP", QGVAR(3R)};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"RHS_US_A2_AirImport", QUOTE(MAIN_ADDON), "RHS_US_A2Port_Armor"};
+        requiredAddons[] = {QUOTE(MAIN_ADDON)};
         author = "AUTHOR";
         VERSION_CONFIG;
     };
